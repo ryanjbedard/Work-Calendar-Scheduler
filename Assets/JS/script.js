32449e31185301e4071b0d9aaf98175ded79655d
rbedard <ryanjbedard@gmail.com>
2023-11-18 18:53:37 -0800

    Relink CSS and JS file.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,28 +2,18 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  var calendar = require("dayjs/plugin/calendar");
-  dayjs.extend(calendar);
-
-  dayjs().calendar(dayjs("2008-01-01"));
-  dayjs().calendar(null, {
-    sameDay: "[Today at] h:mm A", // The same day ( Today at 2:30 AM )
-    nextDay: "[Tomorrow at] h:mm A", // The next day ( Tomorrow at 2:30 AM )
-    nextWeek: "dddd [at] h:mm A", // The next week ( Sunday at 2:30 AM )
-    lastDay: "[Yesterday at] h:mm A", // The day before ( Yesterday at 2:30 AM )
-    lastWeek: "[Last] dddd [at] h:mm A", // Last week ( Last Monday at 2:30 AM )
-    sameElse: "DD/MM/YYYY", // Everything else ( 17/10/2011 )
-  });
-
+  var today = dayjs();
+  $('currentDay').text(today.format('MMM D, YYYY'));
+  console.log(today);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  $("saveBtn").click(function () {
-    $;
-  });
+  // $("saveBtn").click(function () {
+  //   $;
+  // });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
