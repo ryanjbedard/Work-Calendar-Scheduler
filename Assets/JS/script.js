e55912cdfe01e03f15c97e35115bfd9562514b17
rbedard <ryanjbedard@gmail.com>
2023-11-19 21:07:52 -0800

    Add function to change hour color based on current time.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -24,19 +24,21 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  $(function setTense() {
-    $(for(i=0; i<currentHour;i++){
-      $(if currentHour>i) {
-        $(this).addclass('future');
-        $(this).removeclass('past present');
-
-      }
+  $(function hourColor() {
+    $('.time-block').each(function(){
+      var hourBlock = parseInt(this.id);
+      $(this).toggleClass('past', hourBlock < currentHour);
+      $(this).toggleClass('present', hourBlock === currentHour);
+      $(this).toggleClass('future', hourBlock > currentHour);
     })
   });
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  $(function inputSave(){
+
+  })
 
 
   // TODO: Add code to display the current date in the header of the page.
