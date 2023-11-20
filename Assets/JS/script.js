140bb5e3ffa587ede0813ab3f27f38f0b5629fb7
rbedard <ryanjbedard@gmail.com>
2023-11-19 19:27:08 -0800

    Add code for future tense

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,7 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  
+  var today = dayjs();
 
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -11,9 +11,10 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  // $("saveBtn").click(function () {
-  //   $;
-  // });
+  $("button").click(function()) {
+    $("#saveBTn")
+  }
+ 
 
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -22,9 +23,16 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   $(function setTense() {
+    
     var currentHour = dayjs().hour();
-    if (currentHour < )
-  })
+    $(for(i=0; i<currentHour;i++){
+      $(if currentHour>i) {
+        $(this).addclass('future');
+        $(this).removeclass('past');
+
+      }
+    })
+  });
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -32,6 +40,6 @@ $(function () {
 
 
   // TODO: Add code to display the current date in the header of the page.
-  var today = dayjs();
+  
   $('#currentDay').text(today.format('MMM D, YYYY'));
 });
